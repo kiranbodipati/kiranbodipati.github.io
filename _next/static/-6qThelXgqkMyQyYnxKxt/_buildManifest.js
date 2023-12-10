52e3c0bf1c7630d74564d31383a97cc7d4eb7bee
unknown <kiranbodipati@gmail.com>
2023-12-10 23:38:04 +0530

    Updates

--- _next/static/-6qThelXgqkMyQyYnxKxt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/924-5ad97b09bc474922.js","static/chunks/pages/index-25f5e0d0770d56f7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
