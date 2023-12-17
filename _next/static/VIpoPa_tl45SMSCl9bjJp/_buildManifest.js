9ecc220cff0d9b9e75cf99e4962f4bb06e79619e
unknown <kiranbodipati@gmail.com>
2023-12-17 16:38:25 +0530

    Updates

---
+++ _next/static/VIpoPa_tl45SMSCl9bjJp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/924-5ad97b09bc474922.js","static/chunks/pages/index-f32cecf06ec17856.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
